efa5df0e82b982fa2c73685982abcadd96b6ce5c
Mateusz Boryn <mateusz.boryn@gmail.com>
2022-05-16 08:12:56 +0200

    iterator classes

--- src/bezalel/bezalel.py
+++ src/bezalel/bezalel.py
@@ -1,11 +1,116 @@
-import requests, json, time, os
-import pandas as pd
+import requests
+import time
 import logging
 
-def _write_file(df, out_path):
-    logger = logging.getLogger(__name__)
-    logger.info(f"Writing file {out_path} with {len(df)} records.")
-    df.to_parquet(path=out_path, index=False)
+
+class PaginatedApiIterator:
+    def __init__(self,
+                 session: requests.Session,
+                 url: str,
+                 request_page_number_param_name: str,
+                 # response_page_number_field_name: str,
+                 response_page_count_field_name: str,
+                 response_records_field_name: str,
+                 extra_params: dict = {},
+                 extra_headers: dict = {},
+                 start_page_number_from_1=True
+                 ):
+        self.logger = logging.getLogger(__name__)
+        self._session = session
+        self._url = url
+        self._request_page_number_param_name = request_page_number_param_name
+        # self._response_page_number_field_name = response_page_number_field_name
+        self._response_page_count_field_name = response_page_count_field_name
+        self._response_records_field_name = response_records_field_name
+        self._extra_params = extra_params
+        self._request_headers = {
+            # "Content-type": "application/json",
+            "Accept": "application/json",
+            **extra_headers
+        }
+        self._start_page_number_from_1 = start_page_number_from_1
+        self.page_number = 1 if self._start_page_number_from_1 else 0
+        self._completed = False
+
+    def __iter__(self):
+        self.logger.debug(f"Downloading from {self._url}")
+        self.page_number = 1 if self._start_page_number_from_1 else 0
+        self._completed = False
+        return self
+
+    def __next__(self):
+        if self._completed:
+            raise StopIteration
+
+        response = self._session.get(self._url, headers=self._request_headers,
+                                     params={self._request_page_number_param_name: self.page_number, **self._extra_params})
+        response.raise_for_status()
+
+        response_json = response.json()
+        page_count = response_json[self._response_page_count_field_name]
+
+        self.logger.debug(
+            f"Downloaded page {self.page_number} out of {page_count}. Items collected: {len(response_json[self._response_records_field_name])}")
+
+        self.page_number += 1
+
+        if (self._start_page_number_from_1 and self.page_number > page_count) or \
+                (not self._start_page_number_from_1 and self.page_number >= page_count):
+            self._completed = True
+
+        return response_json[self._response_records_field_name]
+
+
+class CursorApiIterator:
+    def __init__(self, session: requests.Session,
+                         url: str,
+                         request_cursor_param_name: str,
+                         response_cursor_field_name: str,
+                         response_records_field_name: str,
+                         extra_params: dict = {},
+                         extra_headers: dict = {}):
+        self.logger = logging.getLogger(__name__)
+        self._session = session
+        self._url = url
+        self._request_cursor_param_name = request_cursor_param_name
+        self._response_cursor_field_name = response_cursor_field_name
+        self._response_records_field_name = response_records_field_name
+        self._extra_params = extra_params
+        self._request_headers = {
+            # "Content-type": "application/json",
+            "Accept": "application/json",
+            **extra_headers
+        }
+        self._completed = False
+        self._next_cursor = None
+        self.page_number = 1
+
+    def __iter__(self):
+        self.logger.debug(f"Downloading from {self._url}")
+        self._completed = False
+        self._next_cursor = None
+        self.page_number = 1
+        return self
+
+    def __next__(self):
+        if self._completed:
+            raise StopIteration
+        params = {**self._extra_params}
+        params[self._request_cursor_param_name] = self._next_cursor
+
+        response = self._session.get(self._url, headers=self._request_headers, params=params)
+        response.raise_for_status()
+
+        response_json = response.json()
+
+        self._next_cursor = response_json.get(self._response_cursor_field_name)
+        self._completed = self._next_cursor is None
+
+        self.logger.debug(
+            f"Downloaded page {self.page_number}. Items collected: {len(response_json[self._response_records_field_name])}")
+        self.page_number += 1
+
+        return response_json[self._response_records_field_name]
 
 
 def prepare_job(session: requests.Session, url: str,
@@ -31,138 +136,12 @@ def prepare_job(session: requests.Session, url: str,
         time.sleep(wait_delay_seconds)
         state_response = session.get(f"{url}/{job_id}", headers=requestHeaders)
         state_response_json = state_response.json()
-        logger.info(state_response_json)
+        logger.debug(state_response_json)
         state = state_response_json[response_state_field_name]
-        logger.info(f"state: {state}")
+        logger.debug(f"state: {state}")
 
     if state not in successful_states:
         raise Exception(f"Job failed with state {state}. Response {state_response_json}")
 
     return job_id
 
-
-def paginated_api_ingestion(session: requests.Session,
-                            url: str,
-                            request_page_number_param_name: str,
-                            response_page_number_field_name: str,
-                            response_page_count_field_name: str,
-                            response_records_field_name: str,
-                            output_path: str,
-                            extra_params: dict = {},
-                            extra_headers: dict = {},
-                            start_page_number_from_1=True,
-                            pages_per_output_file: int = 1000):
-    logger = logging.getLogger(__name__)
-    logger.info(f"Downloading from {url} to {output_path}..")
-
-    requestHeaders = {
-        # "Content-type": "application/json",
-        "Accept": "application/json",
-        **extra_headers
-    }
-
-    per_file_df = None
-
-    i = 1 if start_page_number_from_1 else 0
-    df_per_file_counter = 0
-    file_counter = 0
-    were_column_names_printed = False
-
-    while True:
-        response = session.get(url, headers=requestHeaders, params={request_page_number_param_name: i, **extra_params})
-        response.raise_for_status()
-
-        response_json = response.json()
-        page_number = response_json[
-            response_page_number_field_name] if response_page_number_field_name is not None else i
-        page_count = response_json[response_page_count_field_name]
-
-        logger.info(
-            f"Downloaded page {page_number} out of {page_count}. Items collected: {len(response_json[response_records_field_name])}")
-
-        df = pd.json_normalize(response_json[response_records_field_name])
-        if not were_column_names_printed:
-            logger.info(f"Columns: {df.columns}")
-            were_column_names_printed = True
-
-        # to avoid ValueError: The truth value of a DataFrame is ambiguous.
-        per_file_df = pd.concat([per_file_df, df]) if per_file_df is not None else df
-        df_per_file_counter += 1
-
-        if df_per_file_counter >= pages_per_output_file:
-            _write_file(per_file_df, f"{output_path}/out_{file_counter}.parquet")
-            per_file_df = None
-            df_per_file_counter = 0
-            file_counter += 1
-
-        i += 1
-        if start_page_number_from_1 and i > page_count:
-            break
-        if not start_page_number_from_1 and i >= page_count:
-            break
-
-    if per_file_df is not None:
-        _write_file(per_file_df, f"{output_path}/out_{file_counter}.parquet")
-
-    logger.info("All pages downloaded.")
-
-
-def cursor_api_ingestion(session: requests.Session,
-                         url: str,
-                         request_cursor_param_name: str,
-                         response_cursor_field_name: str,
-                         response_records_field_name: str,
-                         output_path: str,
-                         extra_params: dict = {},
-                         extra_headers: dict = {},
-                         pages_per_output_file: int = 1000):
-    logger = logging.getLogger(__name__)
-    logger.info(f"Downloading from {url} to {output_path}..")
-
-    requestHeaders = {
-        # "Content-type": "application/json",
-        "Accept": "application/json",
-        **extra_headers
-    }
-
-    i = 1
-    per_file_df = None
-    df_per_file_counter = 0
-    file_counter = 0
-    were_column_names_printed = False
-
-    params = {**extra_params}
-
-    while True:
-        response = session.get(url, headers=requestHeaders, params=params)
-        response.raise_for_status()
-
-        response_json = response.json()
-        params[request_cursor_param_name] = response_json.get(response_cursor_field_name)
-
-        logger.info(
-            f"Downloaded page {i}. Items collected: {len(response_json[response_records_field_name])}")
-
-        df = pd.json_normalize(response_json[response_records_field_name])
-        if not were_column_names_printed:
-            logger.info(f"Columns: {df.columns}")
-            were_column_names_printed = True
-
-        # to avoid ValueError: The truth value of a DataFrame is ambiguous.
-        per_file_df = pd.concat([per_file_df, df]) if per_file_df is not None else df
-        df_per_file_counter += 1
-
-        if df_per_file_counter >= pages_per_output_file:
-            _write_file(per_file_df, f"{output_path}/out_{file_counter}.parquet")
-            per_file_df = None
-            df_per_file_counter = 0
-            file_counter += 1
-        if response_cursor_field_name not in response_json:
-            break
-
-        i += 1
-
-    if per_file_df is not None:
-        _write_file(per_file_df, f"{output_path}/out_{file_counter}.parquet")
-
-    logger.info("All pages downloaded.")
