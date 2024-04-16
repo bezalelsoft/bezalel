97b44bf12c4e0f6b771039b70eaa300febe3be36
bezalelsoft <bezalelsoft@gmail.com>
2024-04-16 16:43:39 +0200

    CursorApiIterator fixed, normalize_with_prototype enhancements

--- src/bezalel/impl/CursorApiIterator.py
+++ src/bezalel/impl/CursorApiIterator.py
@@ -1,27 +1,57 @@
 import requests
 import logging
+import typing as t
 
 
 class CursorApiIterator:
-    def __init__(self, session: requests.Session,
-                         url: str,
-                         request_cursor_param_name: str,
-                         response_cursor_field_name: str,
-                         response_records_field_name: str,
-                         extra_params: dict = {},
-                         extra_headers: dict = {}):
+    def __init__(self,
+                 session: requests.Session,
+                 url: str,
+                 method: str,
+                 response_cursor_field_name: str,
+                 response_records_field_name: str,
+                 request_cursor_param_name: t.Optional[str] = None,
+                 request_cursor_field_name: t.Optional[str] = None,
+                 extra_params: t.Optional[dict] = None,
+                 extra_data: t.Optional[dict] = None,
+                 extra_headers: t.Optional[dict] = None,
+                 empty_data_on_next_cursor: bool=False,
+                 payload_handler=None):
+        """
+
+        :param session: requests.Session object (you can set session.auth = (user, passwd) for authentication)
+        :param url: url to call
+        :param method: get or post
+        :param response_cursor_field_name:
+        :param response_records_field_name:
+        :param request_cursor_param_name:
+        :param request_cursor_field_name:
+        :param extra_params: URL params
+        :param extra_data: request payload params
+        :param extra_headers:
+        :param empty_data_on_next_cursor: Flase
+        :param payload_handler: a function that is called just after fetching next page. Takes dict as a param.
+        """
         self.logger = logging.getLogger(__name__)
         self._session = session
         self._url = url
+        self._method = method.upper()
         self._request_cursor_param_name = request_cursor_param_name
+        self._request_cursor_field_name = request_cursor_field_name
         self._response_cursor_field_name = response_cursor_field_name
         self._response_records_field_name = response_records_field_name
         self._extra_params = extra_params
+        self._extra_data = extra_data
         self._request_headers = {
-            # "Content-type": "application/json",
             "Accept": "application/json",
-            **extra_headers
         }
+        if self._request_cursor_field_name:
+            self._request_headers["Content-type"] = "application/json"
+        if extra_headers:
+            for k, v in extra_headers.items():
+                self._request_headers[k] = v
+        self._empty_data_on_next_cursor = empty_data_on_next_cursor
+        self._payload_handler = payload_handler
         self._completed = False
         self._next_cursor = None
         self.page_number = 1
@@ -36,17 +66,39 @@ class CursorApiIterator:
     def __next__(self):
         if self._completed:
             raise StopIteration
-        params = {**self._extra_params}
-        params[self._request_cursor_param_name] = self._next_cursor
+        if self._extra_params or self._request_cursor_param_name:
+            params = {}
+            if self._extra_params:
+                params = {**self._extra_params}
+            params[self._request_cursor_param_name] = self._next_cursor
+        else:
+            params = None
 
-        response = self._session.get(self._url, headers=self._request_headers, params=params)
+        if self._extra_data or self._request_cursor_field_name:
+            data = {}
+
+            if self._extra_data and (not self._empty_data_on_next_cursor or not self._next_cursor):
+                data = {**self._extra_data}
+
+            if self._request_cursor_field_name and self._next_cursor:
+                data[self._request_cursor_field_name] = self._next_cursor
+            data = data.dict()
+        else:
+            data = None
+
+        response = self._session.request(method=self._method, url=self._url, headers=self._request_headers, params=params, json=data)
         response.raise_for_status()
 
         response_json = response.json()
 
-        self._next_cursor = response_json.get(self._response_cursor_field_name)
-        self._completed = self._next_cursor is None
+        if self._payload_handler:
+            self._payload_handler(response_json)
 
+        if self._response_records_field_name not in response_json.keys():
+            raise Exception(f"Failed to get field '{self._response_records_field_name}' from response {response_json}. Request params: '{params}' data: '{data}'")
+
+        self._next_cursor = response_json.get(self._response_cursor_field_name)
+        self._completed = not self._next_cursor
         self.logger.debug(
             f"Downloaded page {self.page_number}. Items collected: {len(response_json[self._response_records_field_name])}")
         self.page_number += 1
--- src/bezalel/impl/normalize_with_prototype.py
+++ src/bezalel/impl/normalize_with_prototype.py
@@ -1,67 +1,106 @@
 
 
-def normalize_with_prototype(prototype, object_to_norm, freestyle_attrs_name="freestyle_attrs", pass_through_paths=[]):
+class NormalizeException(Exception):
+    def __init__(self, path, message):
+        super().__init__(f"{path}: {message}")
+        self._path = path
+        self._message = message
+
+
+def normalize_with_prototype(prototype, object_to_norm, freestyle_attrs_name="freestyle_attrs", pass_through_paths=[], strict_types=True):
     """
     Normalize python dict, so that it has all the fields and only the fields specified in a prototype dict.
 
-    :param prototype: A prototype dict, for example from Swagger doc
-    :param object_to_norm: A dict that comes from API
+    :param prototype: A prototype dict, for example from Swagger doc.
+    :param object_to_norm: A dict that comes from API.
     :param freestyle_attrs_name: it is a name for attribute, when prototype contains a dict like this: "field": {"": ""}
     :param pass_through_paths: list of paths which will be passed through without normalization (for example dicts with
         unpredicted structure, that will be handled after normalization)
+    :param strict_types: if True, then fail on types mismatch between prototype and object_to_norm.
+        If False, function will attempt to parse string, i.e. "123.45" to 123.45 float.
+        It will fail on parsing errors anyway.
     :return:
     """
     def normalize_with_prototype_rec(prototype, object_to_norm, path_info, prototype_path):
-        if prototype_path in pass_through_paths:
-            return object_to_norm
-        if isinstance(prototype, dict):
-            normalized_object = {}
-            if object_to_norm is None:
-                object_to_norm = {}
-            if not isinstance(object_to_norm, dict):
-                raise Exception(f"{path_info}: object_to_norm is not dict")
-            for k, v in prototype.items():
-                if k != "":
-                    normalized_object[k] = normalize_with_prototype_rec(prototype[k], object_to_norm.get(k), path_info=f"{path_info}.{k}", prototype_path=f"{prototype_path}.{k}" if prototype_path != "" else k)
-                else:
-                    normalized_object[freestyle_attrs_name] = [
-                        {"key": key, "value": value} for key, value in object_to_norm.items() if key not in prototype.keys() or key == ""
-                    ]
-            return normalized_object
-        elif isinstance(prototype, list):
-            if object_to_norm is None:
-                object_to_norm = []
-            if not isinstance(object_to_norm, list):
-                raise Exception(f"{path_info}: object_to_norm is not list")
-            return [normalize_with_prototype_rec(prototype[0], e, path_info=f"{path_info}[{idx}]", prototype_path=prototype_path) for idx, e in enumerate(object_to_norm)]
-        elif isinstance(prototype, str):
-            if object_to_norm is None:
-                return None
-            if type(prototype) != type(object_to_norm):
-                raise Exception(f"{path_info}: type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
-            return object_to_norm
-        elif isinstance(prototype, bool):
-            if object_to_norm is None:
-                return None
-            if type(prototype) != type(object_to_norm):
-                raise Exception(f"{path_info}: type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
-            return object_to_norm
-        elif isinstance(prototype, int):
-            if object_to_norm is None:
-                return None
-            if not isinstance(prototype, (int, float)):
-                raise Exception(f"{path_info}: expecting int or float, got {type(object_to_norm)} (of value {object_to_norm})")
-            if type(object_to_norm) != int:
-                object_to_norm = int(object_to_norm)
-            return object_to_norm
-        elif isinstance(prototype, float):
-            if object_to_norm is None:
-                return None
-            if not isinstance(prototype, (int, float)):
-                raise Exception(f"{path_info}: expecting int or float, got {type(object_to_norm)} (of value {object_to_norm})")
-            if type(object_to_norm) != float:
-                object_to_norm = float(object_to_norm)
-            return object_to_norm
-        else:
-            raise Exception(f"{path_info}: prototype data type not supported: {type(prototype)}")
+        try:
+            if prototype_path in pass_through_paths:
+                return object_to_norm
+            if isinstance(prototype, dict):
+                normalized_object = {}
+                if object_to_norm is None:
+                    object_to_norm = {}
+                if not isinstance(object_to_norm, dict):
+                    raise NormalizeException(path_info, f"object_to_norm is not dict")
+                for k, v in prototype.items():
+                    if k != "":
+                        normalized_object[k] = normalize_with_prototype_rec(prototype[k], object_to_norm.get(k), path_info=f"{path_info}.{k}", prototype_path=f"{prototype_path}.{k}" if prototype_path != "" else k)
+                    else:
+                        normalized_object[freestyle_attrs_name] = [
+                            {"key": key, "value": value} for key, value in object_to_norm.items() if key not in prototype.keys() or key == ""
+                        ]
+                return normalized_object
+            elif isinstance(prototype, list):
+                if object_to_norm is None:
+                    object_to_norm = []
+                if not isinstance(object_to_norm, list):
+                    raise NormalizeException(path_info, f"object_to_norm is not list")
+                return [normalize_with_prototype_rec(prototype[0], e, path_info=f"{path_info}[{idx}]", prototype_path=prototype_path) for idx, e in enumerate(object_to_norm)]
+            elif isinstance(prototype, str):
+                if object_to_norm is None:
+                    return None
+                if type(prototype) != type(object_to_norm):
+                    if strict_types:
+                        raise NormalizeException(path_info, f"type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
+                    else:
+                        return str(object_to_norm)
+                return object_to_norm
+            elif isinstance(prototype, bool):
+                if object_to_norm is None:
+                    return None
+                if type(prototype) != type(object_to_norm):
+                    if strict_types or not isinstance(object_to_norm, str):
+                        raise NormalizeException(path_info, f"type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
+                    else:
+                        if object_to_norm.lower().strip() in {'1', 'true', 'yes'}:
+                            return True
+                        elif object_to_norm.lower().strip() in {'0', 'false', 'no'}:
+                            return False
+                        return None
+                return object_to_norm
+            elif isinstance(prototype, int):
+                if object_to_norm is None:
+                    return None
+                if not isinstance(object_to_norm, int):
+                    if strict_types or not isinstance(object_to_norm, (str, float)):
+                        raise NormalizeException(path_info, f"type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
+                    else:
+                        if isinstance(object_to_norm, str) and object_to_norm.strip() == "":
+                            return None
+                        object_to_norm = int(object_to_norm)
+                return object_to_norm
+            elif isinstance(prototype, float):
+                if object_to_norm is None:
+                    return None
+                if not isinstance(prototype, (int, float)):
+                    if strict_types or not isinstance(object_to_norm, str):
+                        raise NormalizeException(path_info, f"type(prototype) != type(object_to_norm): {type(prototype)} != {type(object_to_norm)} (of value {object_to_norm})")
+                    else:
+                        if isinstance(object_to_norm, str) and object_to_norm.strip() == "":
+                            return None
+                        try:
+                            object_to_norm = float(object_to_norm)
+                        except Exception as e:
+                            raise NormalizeException(path_info, f"can't parse float {object_to_norm}: {e}")
+                if type(object_to_norm) != float:
+                    try:
+                        object_to_norm = float(object_to_norm)
+                    except Exception as e:
+                        raise NormalizeException(path_info, f"can't parse float {object_to_norm}: {e}")
+                return object_to_norm
+            else:
+                raise NormalizeException(path_info, f"prototype data type not supported: {type(prototype)}")
+        except NormalizeException as e:
+            raise
+        except Exception as e:
+            NormalizeException(path_info, str(e))
     return normalize_with_prototype_rec(prototype, object_to_norm, "", "")
